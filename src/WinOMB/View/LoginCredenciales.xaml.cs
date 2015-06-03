5c1897dbb35bd42449eec75d68d5e5bc8de30544
Octavio92 <octi_danieli@hotmail.com>
2015-06-02 21:07:11 -0300

    Clase 02-06-2015

---
+++ src/WinOMB/View/LoginCredenciales.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowsOMB.ViewModel;
+
+namespace WindowsOMB.View
+{
+    /// <summary>
+    /// Interaction logic for LoginCredenciales.xaml
+    /// </summary>
+    public partial class LoginCredenciales : UserControl
+    {
+        private LoginViewModel _viewModel;
+
+        // puede pasarse como argumento a LoginCredenciales una Interfaz, en vez de LoginViewModel...
+        public LoginCredenciales( LoginViewModel vm)
+        {
+            InitializeComponent();
+            _viewModel = vm;
+            this.DataContext = _viewModel;
+        }
+
+        private void LoginLoaded(object sender, RoutedEventArgs e)
+        {
+            txtUsuario.Focus();
+        }
+
+    }
+}
---
+++ src/WinOMB/View/winLogin.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WindowsOMB.View;
+using WindowsOMB.ViewModel;
+
+namespace WindowsOMB
+{
+    /// <summary>
+    /// Interaction logic for winLogin.xaml
+    /// </summary>
+    public partial class winLogin : Window
+    {
+        // evitar esto, que la ventana tenga que instanciar el VM, podria usarse Locate...
+        private LoginViewModel _viewModel;
+        public winLogin()
+        {
+            InitializeComponent();
+
+            _viewModel = new LoginViewModel();
+            this.loginContent.Content = new LoginCredenciales(_viewModel);
+        }
+    }
+}
