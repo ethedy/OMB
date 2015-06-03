c097ff6aabb6c49e1fe18b67314a3d91511dc217
ethedy <ethedy@gmail.ccom>
2015-06-03 19:53:40 -0300

    Fin Clase 02/06

---
+++ src/WinOMB/View/winLogin.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using WindowsOMB.ViewModel;
+
+namespace WindowsOMB.View
+{
+  /// <summary>
+  /// Interaction logic for winLogin.xaml
+  /// </summary>
+  public partial class winLogin : Window
+  {
+    private LoginViewModel _viewModel;
+
+    public winLogin()
+    {
+      InitializeComponent();
+
+      _viewModel = new LoginViewModel();
+      this.loginContent.Content = new LoginCredenciales(_viewModel);
+    }
+  }
+}
---
+++ src/WinOMB/ViewModel/LoginViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+
+namespace WindowsOMB.ViewModel
+{
+  public class LoginViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private Usuario _usuario;
+    public Usuario UsuarioModel
+    {
+      get { return _usuario; }
+      set
+      {
+        _usuario = value;
+        OnPropertyChanged("UsuarioModel");
+      }
+    }
+
+    public LoginViewModel()
+    {
+      UsuarioModel = new Usuario();
+      UsuarioModel.Login = "mburns";
+    }
+
+    private void OnPropertyChanged(string propiedad)
+    {
+      if (PropertyChanged != null)
+        PropertyChanged(this, new PropertyChangedEventArgs(propiedad));
+    }
+  }
+
+
+}
